a9bcb7de61fb1d4156ebab7a18a37a93f6d856f9
netb258 <netb258@abv.bg>
2020-04-30 17:47:07 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pong "0.1.0-SNAPSHOT"
   :description "The classic game Pong written in Clojure."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/netb258/clj-pong"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
